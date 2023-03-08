15269cbd7fb913fecda2a34b4519c168e4a4508f
MDD1299 <mdd.workmail@gmail.com>
2023-03-08 10:37:27 +0800

    Update

--- app.py
+++ app.py
@@ -9,3 +9,20 @@ def index():
 @app.route('/about/')
 def about():
     return render_template('about.html')
+
+@app.route('/ml')
+def ml():
+    return render_template('ml.html')
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    select_ = request.form.get('pos_lvl')
+    exp_ = request.form.get('experience')
+    # print(select_)
+    # print('form val: ', request.form.values())
+    int_features = [select_, exp_]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    res = prediction.item()
+
+    return render_template('ml.html', prediction_text='Expected Salary Rate should be $ {}'.format(res) )
